06aa39c9086859b8d9b47393a4402a94daa01368
Beloha18 <Scottallemand7@gmail.com>
2026-02-20 19:52:45 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,27 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const createTypeClient= require('./middleware/TypeClient');
-
-app.use(CORS())
-app.use(express.json());
-app.use(createTypeClient);
-
-mongoose.connect(process.env.MONGO_URI)
-    .then(() => {console.log("MongoDB Connected")})
-    .catch((error) => {
-        console.log(error);})
-app.use(createTypeClient);
-// Routes
-app.use("/LocaleCM", require("./routes/LocaleRoute"));
-app.use("/VisiteCM", require("./routes/visiteRoutes"));
-app.use("/ReservationCM", require("./routes/ReservationLocalRoute"));
-app.use('/auth', require('./routes/authRoutes'));
-app.listen(PORT, () => console.log(`Serveur démarré sur le port 
-${PORT}`));
-
-
-
-=======
 const cors = require('cors');
 const path = require('path');
 const dotenv = require('dotenv');
@@ -41,6 +20,11 @@ mongoose.connect(process.env.MONGO_URI)
         app.use('/auth', require('./routes/authRoutes'));
         app.use('/boutique', require('./routes/boutiqueRoutes'));
         app.use('/categorie', require('./routes/categorieRoutes'));
+        app.use(createTypeClient);
+        app.use("/LocaleCM", require("./routes/LocaleRoute"));
+        app.use("/VisiteCM", require("./routes/visiteRoutes"));
+        app.use("/ReservationCM", require("./routes/ReservationLocalRoute"));
+        app.use('/auth', require('./routes/authRoutes'));
 
         app.listen(PORT, () => {
             console.log(`Serveur démarré sur le port ${PORT}`);
